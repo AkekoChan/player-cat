3df4731037ad12ec7d72be4b3fbe3be863e6e799
Akeko. <artapp.theo@gmail.com>
2025-02-03 17:22:25 +0100

    test: add properties props

--- src/__tests__/AnimatedBackground.test.tsx
+++ src/__tests__/AnimatedBackground.test.tsx
@@ -1,22 +1,34 @@
 import { render, screen } from '@testing-library/react';
-import { describe, expect, test, vi } from 'vitest';
+import { describe, expect, it, vi } from 'vitest';
 import AnimatedBackground from '../components/animated-background/AnimatedBackground';
 
 describe('AnimatedBackground', () => {
-  test('sould render canvas correctly ', () => {
-    render(<AnimatedBackground width={500} height={300} />);
+  it('should render canvas correctly', () => {
+    render(
+      <AnimatedBackground
+        width={500}
+        height={300}
+        properties={{ bgColor: '#F06292', lineWidth: 6, lineColor: '#FEFEFE' }}
+      />
+    );
     const canvas = screen.getByTestId('canvas');
     expect(canvas).toBeInTheDocument();
   });
 
-  test('should have correct dimensions', () => {
-    render(<AnimatedBackground width={500} height={300} />);
+  it('should have correct dimensions', () => {
+    render(
+      <AnimatedBackground
+        width={500}
+        height={300}
+        properties={{ bgColor: '#F06292', lineWidth: 6, lineColor: '#FEFEFE' }}
+      />
+    );
     const canvas = screen.getByTestId('canvas');
     expect(canvas).toHaveStyle('width: 500px');
     expect(canvas).toHaveStyle('height: 300px');
   });
 
-  test('should get 2D context', () => {
+  it('should get 2D context', () => {
     const contextMock = {
       fillRect: vi.fn(),
       clearRect: vi.fn(),
@@ -35,11 +47,17 @@ describe('AnimatedBackground', () => {
       () => contextMock
     );
 
-    render(<AnimatedBackground width={500} height={300} />);
+    render(
+      <AnimatedBackground
+        width={500}
+        height={300}
+        properties={{ bgColor: '#F06292', lineWidth: 6, lineColor: '#FEFEFE' }}
+      />
+    );
     expect(contextMock.fillRect).toHaveBeenCalled();
   });
 
-  test('should draw on the canvas', () => {
+  it('should draw on the canvas', () => {
     const contextMock = {
       fillRect: vi.fn(),
       clearRect: vi.fn(),
@@ -58,18 +76,31 @@ describe('AnimatedBackground', () => {
       () => contextMock
     );
 
-    render(<AnimatedBackground width={500} height={300} />);
+    render(
+      <AnimatedBackground
+        width={500}
+        height={300}
+        properties={{ bgColor: '#F06292', lineWidth: 6, lineColor: '#FEFEFE' }}
+      />
+    );
 
     expect(contextMock.fillRect).toHaveBeenCalled();
   });
-  test('should request animation frame', () => {
+
+  it('should request animation frame', () => {
     const mockCallback = vi.fn();
 
     const requestAnimationFrameMock = vi
       .spyOn(window, 'requestAnimationFrame')
       .mockImplementationOnce(mockCallback);
 
-    render(<AnimatedBackground width={500} height={300} />);
+    render(
+      <AnimatedBackground
+        width={500}
+        height={300}
+        properties={{ bgColor: '#F06292', lineWidth: 6, lineColor: '#FEFEFE' }}
+      />
+    );
 
     expect(requestAnimationFrameMock).toHaveBeenCalledTimes(1);
     expect(mockCallback).toHaveBeenCalledTimes(1);
